4852449f7e5faa263ee9ebca8495e8756601b220
Jay Lindquist <jay.lindquist@code42.com>
2015-02-02 12:29:02 -0600

    Initial commit

---
+++ src/main/groovy/ui/UiApplication.groovy
@@ -0,0 +1,12 @@
+package ui
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class UiApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run UiApplication, args
+    }
+}
